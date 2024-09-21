7ddb5c97ca9a9589e3a7f04b118457f7089ad01a
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-09-21 14:05:55 -0700

    Add an API call to llama

--- housinghoudini/src/app/api/route.js
+++ housinghoudini/src/app/api/route.js
@@ -3,13 +3,30 @@ import { NextResponse } from 'next/server';
 import Replicate from 'replicate';
 
 export async function POST(request) {
+  const { studentPreferences, listingDesc } = await request.json();
+
   const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN // Ensure you have your API token set in environment variables
   });
 
   const input = {
     top_p: 1,
-    prompt: "Can you write a poem about open source machine learning? Let's make it in the style of E. E. Cummings.",
+    prompt: `I will provide you with a student's demographic profile and preferences along with a student housing listing. Your purpose is to provide a compatibility rating for matching the student with the housing. 
+
+Your response should be formatted as follows:
+
+"""
+# Compatibility Rating: x/10
+
+## Pros: 1-5 sentences reflecting compatibility rating
+## Cons: 1-5 sentences reflecting compatibility rating
+""
+
+Student:
+${studentPreferences}
+
+Listing:
+${listingDesc}`,
     temperature: 0.5,
     max_new_tokens: 500,
     min_new_tokens: -1
