ffb8a9c0d61848a30611ee2a8ba05a6c628b5b36
noahstew <njstewart22@icloud.com>
2024-09-21 14:19:17 -0700

    Moving handleClick to UserDataForm and attaching to button

--- housinghoudini/src/components/UserDataForm.jsx
+++ housinghoudini/src/components/UserDataForm.jsx
@@ -18,17 +18,43 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-function UserDataForm() {
+function UserDataForm({clickHandler}) {
+  const handleClick = async () => {
+    const studentPreferences = "21, male. Budget: $1200 a month. Prefers furnished room and likes movies";
+    const listingDesc = "A cozy 2-bedroom apartment with a beautiful view near a movie theatre";
 
-      return (
-        <div>
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
+    try {
+      const res = await fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          studentPreferences,
+          listingDesc
+        })
+      });
+
+      const data = await res.json();
+      // TODO: pass to results pages
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    console.log( "Button clicked" );
+  };
+
+
+  return (
+    <div className="flex items-center justify-center">
+      <Card className="w-[80%]">
+        {/* Commented out, will add if feel needed */}
+        {/* <CardHeader>
+          <CardTitle>Edit your </CardTitle>
           <CardDescription>
             Deploy your new project in one-click.
           </CardDescription>
-        </CardHeader>
+        </CardHeader> */}
+        <CardHeader></CardHeader> {/* Added for default padding */}
         <CardContent>
           <form>
             <div className="grid w-full items-center gap-4">
@@ -53,9 +79,8 @@ function UserDataForm() {
             </div>
           </form>
         </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
+        <CardFooter className="flex justify-center">
+          <Button onClick={handleClick}>Show me some bedder rentals!</Button>
         </CardFooter>
       </Card>
     </div>
