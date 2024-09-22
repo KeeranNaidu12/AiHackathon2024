ad2abeb5f3b331988ad14f24d54b0c4c02d687d4
noahstew <njstewart22@icloud.com>
2024-09-21 17:58:10 -0700

    fixing merge error

--- housinghoudini/src/app/results/page.js
+++ housinghoudini/src/app/results/page.js
@@ -1,11 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 import Header from "@/components/Header";
 import ResultsOutput from "@/components/ResultsOutput";
 import Footer from "@/components/Footer";
-=======
-import ResultsOutput from "./ResultsOutput";
->>>>>>> 19dcc08a2aa4f62de1cf83e370b5ab6e564af287
 
 function Results(){
   return(
