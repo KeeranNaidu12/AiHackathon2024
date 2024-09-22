40803681438467c1ed039817561ca7df5495d57c
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-09-22 14:06:14 -0700

    Solves #3, and refactor

--- housinghoudini/src/components/ResultsOutput.jsx
+++ housinghoudini/src/components/ResultsOutput.jsx
@@ -6,10 +6,10 @@ function ResultsOutput({ housingList }) {
   console.log(housingList);
   return (
     <div className="p-4 w-[80%] mx-auto">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">Find Housing</h1>
+      <h1 className="text-3xl font-bold text-white-800 mb-6">Compatibility Results</h1>
       <div className="housinglist">
         {housingList.length === 0 ? (
-          <p>No matches found.</p>
+          <p>Loading</p>
         ) : (
           housingList.map((housing, index) => (
             <HousingCardComponent
