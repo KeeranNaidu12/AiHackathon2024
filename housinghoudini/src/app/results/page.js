372135c6010b69ecf35d8e537a6c3b5596f0802c
noahstew <njstewart22@icloud.com>
2024-09-21 20:49:12 -0700

    Refactored & Complete Components for UserDataForm

--- housinghoudini/src/app/results/page.js
+++ housinghoudini/src/app/results/page.js
@@ -3,13 +3,14 @@ import Header from "@/components/Header";
 import ResultsOutput from "@/components/ResultsOutput";
 import Footer from "@/components/Footer";
 
-function Results(){
-  return(
-  <div>
-    <Header/>
-   <ResultsOutput housingdata={housingData}/>
-  <Footer/>
-  </div>);
+function Results() {
+  return (
+    <div>
+      <Header />
+      <ResultsOutput housingdata={housingData} />
+      <Footer />
+    </div>
+  );
 }
 
-export default Results; 
+export default Results;
